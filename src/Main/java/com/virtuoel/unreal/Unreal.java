5f27d3930984140e0f5f086e317cb5ef5e7285cd
Virtuoel <virtuoel@gmail.com>
2014-08-28 21:51:26 -0400

    Base mod class

---
+++ src/Main/java/com/virtuoel/unreal/Unreal.java
@@ -0,0 +1,28 @@
+package com.virtuoel.unreal;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="unreal",name="Unreal",version="1.7.10-1.0")
+public class Unreal
+{
+	@Mod.EventHandler
+	public void PreInit(FMLPreInitializationEvent event)
+	{
+		
+	}
+	
+	@Mod.EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+		
+	}
+	
+	@Mod.EventHandler
+	public void PostInit(FMLPostInitializationEvent event)
+	{
+		
+	}
+}

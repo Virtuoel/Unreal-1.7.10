41d2f7a373116a9c72d08f43ffbcb1aefa7e9fd2
Virtuoel <virtuoel@gmail.com>
2014-08-29 20:04:21 -0400

    2nd Item
    Trying different way of registering item

--- src/Main/java/com/virtuoel/unreal/init/ModItems.java
+++ src/Main/java/com/virtuoel/unreal/init/ModItems.java
@@ -2,9 +2,11 @@ package com.virtuoel.unreal.init;
 
 import com.virtuoel.unreal.item.ItemIngotTitanium;
 import com.virtuoel.unreal.item.ItemUnreal;
+import com.virtuoel.unreal.reference.Reference;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 
+@GameRegistry.ObjectHolder(Reference.MOD_ID)
 public class ModItems
 {
 	public static final ItemUnreal ingotTitanium = new ItemIngotTitanium();
@@ -12,6 +14,7 @@ public class ModItems
 	public static void init()
 	{
 		GameRegistry.registerItem(ingotTitanium, "ingotTitanium");
+		GameRegistry.registerItem(new ItemUnreal().setUnlocalizedName("gemTarydium"), "gemTarydium");
 		
 	}
 }

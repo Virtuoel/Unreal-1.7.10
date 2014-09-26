37b9676b29bc89ecbe9e5999f8d6724016da2b93
Virtuoel <virtuoel@gmail.com>
2014-09-26 00:14:06 -0400

    Started work on projectile base

---
+++ src/Main/java/com/virtuoel/unreal/entity/EntityProjectileUnreal.java
@@ -0,0 +1,35 @@
+package com.virtuoel.unreal.entity;
+
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.projectile.EntityThrowable;
+import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.world.World;
+
+public class EntityProjectileUnreal extends EntityThrowable
+{
+	
+	
+	public EntityProjectileUnreal(World p_i1776_1_) {
+		super(p_i1776_1_);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	public EntityProjectileUnreal(World p_i1778_1_, double p_i1778_2_,	double p_i1778_4_, double p_i1778_6_)
+	{
+		super(p_i1778_1_, p_i1778_2_, p_i1778_4_, p_i1778_6_);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public EntityProjectileUnreal(World p_i1777_1_, EntityLivingBase p_i1777_2_) {
+		super(p_i1777_1_, p_i1777_2_);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void onImpact(MovingObjectPosition p_70184_1_)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+}

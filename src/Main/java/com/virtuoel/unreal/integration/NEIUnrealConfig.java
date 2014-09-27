97ea305234335f517ccc952da4e6bb0d843de466
Virtuoel <virtuoel@gmail.com>
2014-09-26 23:13:04 -0400

    Started work on NEI integration

---
+++ src/Main/java/com/virtuoel/unreal/integration/NEIUnrealConfig.java
@@ -0,0 +1,51 @@
+package com.virtuoel.unreal.integration;
+
+import net.minecraft.item.ItemStack;
+
+import com.virtuoel.unreal.init.ModBlocks;
+import com.virtuoel.unreal.init.ModItems;
+import com.virtuoel.unreal.reference.Reference;
+
+import codechicken.nei.api.API;
+import codechicken.nei.api.IConfigureNEI;
+
+public class NEIUnrealConfig implements IConfigureNEI
+{
+
+	@Override
+	public void loadConfig()
+	{
+		API.hideItem(new ItemStack(ModBlocks.blockUUTC));
+		API.hideItem(new ItemStack(ModBlocks.blockTarydiumAlloyUnrefined));
+		API.hideItem(new ItemStack(ModBlocks.blockTarydiumAlloyRefined));
+		API.hideItem(new ItemStack(ModBlocks.oreBedrockiumHidden));
+		API.hideItem(new ItemStack(ModBlocks.oreBedrockium));
+		API.hideItem(new ItemStack(ModBlocks.blockBedrockium));
+		API.hideItem(new ItemStack(ModBlocks.blockEnderCrystal));
+
+		API.hideItem(new ItemStack(ModItems.swordTarydiumAlloyRefined));
+		API.hideItem(new ItemStack(ModItems.pickaxeTarydiumAlloyRefined));
+		API.hideItem(new ItemStack(ModItems.axeTarydiumAlloyRefined));
+		API.hideItem(new ItemStack(ModItems.shovelTarydiumAlloyRefined));
+		API.hideItem(new ItemStack(ModItems.hoeTarydiumAlloyRefined));
+		API.hideItem(new ItemStack(ModItems.rodTarydiumAlloyUnrefined));
+		API.hideItem(new ItemStack(ModItems.rodTarydiumAlloyRefined));
+		API.hideItem(new ItemStack(ModItems.dustTarydiumAlloyRefined));
+		API.hideItem(new ItemStack(ModItems.dustTarydiumAlloyUnrefined));
+		API.hideItem(new ItemStack(ModItems.ingotBedrockium));
+		API.hideItem(new ItemStack(ModItems.nuggetBedrockium));
+	}
+
+	@Override
+	public String getName()
+	{
+		return Reference.MOD_NAME;
+	}
+
+	@Override
+	public String getVersion()
+	{
+		return Reference.VERSION;
+	}
+	
+}
